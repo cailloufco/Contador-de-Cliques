1cf6476eb29132054bd8e2add10cc859ca608ca8
Caio <caioextreme000@gmail.com>
2025-12-30 00:42:37 -0300

    Contador Funcional

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 
 let ContadorInicio = 0
 
+//oq vai ser exibido
+let textNoSite = document.getElementById("BotaoClique")
+textNoSite.style.font = "normal 25pt Arial"
 
+
+function Reset(){
+textNoSite.innerHTML = ("Clique Aqui!")
+ContadorInicio = 0
+}
 
 function Adição(){
 ContadorInicio ++
 console.log(`${ContadorInicio}`)
+textNoSite.innerHTML = ContadorInicio
 }
